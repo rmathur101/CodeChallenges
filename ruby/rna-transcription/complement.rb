d4e9e02202cdafeae6d23b0e45ec8c0824e34d36
Rohan Mathur <rmathur101@gmail.com>
2014-06-14 09:02:22 -0500

    all tests pass after refactoring

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -4,28 +4,38 @@
 =end
 
 class Complement
+
 	def self.of_dna(strand)
-		
-		strand.each_char do |char|
-
-		end
-
+		return transcribe_strand(strand, "DNA")
 	end
 
-	def transcribe(char)
-		case char
-		when "G"
+	def self.of_rna(strand)
+		return transcribe_strand(strand, "RNA")
+	end
+
+	def self.transcribe_strand(strand, strand_type)
+		string_complement = ""
+		strand.each_char do |char|
+			string_complement += transcribe_char(char, strand_type)
+		end
+		return string_complement
+	end
+
+	def self.transcribe_char(char, strand_type)
+		case 
+		when "G" == char
 			return "C"
-		when "C"
+		when "C" == char
 			return "G"
-		when "T"
+		when "T" == char
 			return "A"
-		when "A"
+		when "A" == char && strand_type == "DNA"
 			return "U"
+		when "A" == char && strand_type == "RNA"
+			return "T"
+		when 'U' == char
+			return "A"
 		end
 	end
 
 end
-
-this_thing = Complement.new
-p this_thing.transcribe("A")

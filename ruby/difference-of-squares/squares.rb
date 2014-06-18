3daf1e2f3e878cfb2af9c367c95f2e6394f87c90
Rohan Mathur <rmathur101@gmail.com>
2014-06-17 20:35:54 -0400

    abstracted creation of array of numbers

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -1,12 +1,20 @@
+=begin Pseudocode
+how to make this code dry.
+information about num CHECK
+information about creating array => this should be in initialize so array can be changed 
+
+=end
+
 class Squares
 
 def initialize(num)
 	@num = num
+	@array_nums = [*1..@num]
 end
 
 def square_of_sums
 	total = 0
-	[*1..@num].each do |n|
+	@array_nums.each do |n|
 		total += n
 	end
 	return total**2
@@ -14,7 +22,7 @@ end
 
 def sum_of_squares
 	total = 0
-	[*1..@num].each do |n|
+	@array_nums.each do |n|
 		total += n**2
 	end
 	return total

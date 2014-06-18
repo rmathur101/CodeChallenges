901b2dea5185916dc930c66daa576ad8b0502440
Rohan Mathur <rmathur101@gmail.com>
2014-06-17 22:14:57 -0400

    Version 2 all tests pass; logic absracted into methods

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,30 @@
 class Hamming
 	
 	def self.compute(strand1, strand2)
-		strand1_array = strand1.downcase.split("")
-		strand2_array = strand2.downcase.split("")
 
-		return 0 if strand1_array.length != strand2_array.length
+		standardize_strands(strand1, strand2)
+		
+		if check_lengths == false
+			return 0
+		else
+			return get_hamming_difference
+		end
 
+	end
+
+	def self.standardize_strands(strand1, strand2)
+		@strand1_array = strand1.downcase.split("")
+		@strand2_array = strand2.downcase.split("") 
+	end
+
+	def self.check_lengths
+		@strand1_array.length == @strand2_array.length ? true : false
+	end
+
+	def self.get_hamming_difference
 		hamming_difference = 0
-		strand1_array.each_with_index do |letter, index|		
-			if letter != strand2_array[index]
+		@strand1_array.each_with_index do |letter, index|		
+			if letter != @strand2_array[index]
 				hamming_difference += 1
 			end  
 		end
@@ -17,16 +33,3 @@ class Hamming
 
 end
 
-
-# def get_hamming_difference(strand1, strand2)
-# 	strand1_array = strand1.downcase.split("")
-# 	strand2_array = strand2.downcase.split("")
-
-# 	hamming_difference = 0
-# 	strand1_array.each_with_index do |letter, index|		
-# 		if letter != strand2_array[index]
-# 			hamming_difference += 1
-# 		end  
-# 	end
-# 	return hamming_difference
-# end

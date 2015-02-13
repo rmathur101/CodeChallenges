003b679d6ec4fcb433eb490d4d5ceff3dde8ffa3
Rohan Mathur <rmathur101@gmail.com>
2015-02-13 16:24:04 -0600

    refactored to use += instead of full expression

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -5,7 +5,7 @@ def word_count(string):
 	words_array = re.findall('\S+', string)
 	for word in words_array:
 		if word in words_dict:
-			words_dict[word] = words_dict[word] + 1
+			words_dict[word] += 1
 		else:
 			words_dict[word] = 1
 	return words_dict
